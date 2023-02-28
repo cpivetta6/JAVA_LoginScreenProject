542686b6c1fe0a47454d56ccdb5f79f6b2660761
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-28 10:58:20 +0100

    Class user and endpoint post

--- src/main/java/com/caiopivetta6/controller/indexController.java
+++ src/main/java/com/caiopivetta6/controller/indexController.java
@@ -2,15 +2,30 @@ package com.caiopivetta6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.caiopivetta6.model.User;
 
 @Controller
 public class indexController {
 
-	@GetMapping("/style")
-	public String style() {
+	@GetMapping("/main")
+	public String index() {
 		return "index";
 	}
 	
+	@GetMapping("/register.html")
+	public String register() {
+		return "register";
+	}
+	
+	
+	@PostMapping("/register")
+	public String userRegistration(@ModelAttribute User user) {
+		System.out.println(user.toString());
+		return "index";
+	}
 	
 	
 	
